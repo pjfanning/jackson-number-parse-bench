f86dfcbead34cf60a6aeb0b31f4782dbfd582c23
PJ Fanning <pjfanning@users.noreply.github.com>
2022-12-21 22:14:04 +0100

    add long parse benchmark

---
+++ src/jmh/java/org/example/jackson/bench/LongParserBench.java
@@ -0,0 +1,41 @@
+package org.example.jackson.bench;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.io.NumberInput;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.apache.commons.io.IOUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class LongParserBench extends BenchmarkLauncher {
+
+    private static final int LEN = 1000;
+    private static final String[] LONGS = new String[LEN];
+
+    static {
+        Random random = new Random(0L);
+        for (int i = 0; i < LEN; i++) {
+            LONGS[i] = Long.toString(random.nextLong());
+        }
+    }
+
+    @Benchmark
+    public void existingJacksonParse(Blackhole blackhole) throws Exception {
+        for (int i = 0; i < LEN; i++) {
+            blackhole.consume(NumberUtil.jacksonParseLong(LONGS[i]));
+        }
+    }
+
+    @Benchmark
+    public void alternateJacksonParse(Blackhole blackhole) throws Exception {
+        for (int i = 0; i < LEN; i++) {
+            blackhole.consume(NumberUtil.alternateParseLong(LONGS[i]));
+        }
+    }
+}
