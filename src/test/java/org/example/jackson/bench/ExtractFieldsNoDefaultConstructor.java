4acef1260ef3cbfcc2a2514b0a690d2a42792ecc
PJ Fanning <pjfanning@users.noreply.github.com>
2023-01-10 12:59:27 +0100

    Update ExtractFieldsNoDefaultConstructor.java

--- src/test/java/org/example/jackson/bench/ExtractFieldsNoDefaultConstructor.java
+++ src/test/java/org/example/jackson/bench/ExtractFieldsNoDefaultConstructor.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExtractFieldsNoDefaultConstructor {
     private final String s;
     private final int i;
